ac0991dfa166a960af4ad3995bd46a4e6b1daa0d
devs001 <devendersandhu001@gmail.com>
2021-03-22 21:57:20 +0530

    try2

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: daphne -b 0.0.0.0 -p 8001 blogd.asgi:application
+web: daphne blogd.asgi:application --post $PORT --bind 0.0.0.0 -v2
 chatworker: python manage.py runworker --settings-blogd.settings -v2
--- blogd/asgi.py
+++ blogd/asgi.py
@@ -1,30 +1,8 @@
-"""
-ASGI config for blogd project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
-"""
 
 import os
+import django
+from channels.routing import get_default_application
 
-from django.core.asgi import get_asgi_application
-from django.urls import re_path
-from channels.routing import ProtocolTypeRouter,URLRouter,get_default_application
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blogd.settings')
-from channels.auth import AuthMiddlewareStack
-from .users import consumers
-django_asgi_app = get_asgi_application()
-
-application=ProtocolTypeRouter(
-        {
-            "http": django_asgi_app,
-            'websocket': AuthMiddlewareStack(
-                URLRouter([
-                    re_path(r'ws/userschatroom/(?P<Susername>[\w@.]+)/(?P<Rusername>[\w@.]+)/$',consumers.ChatConsumer.as_asgi()),
-                    re_path(r'ws/notifications/$', consumers.ChatConsumer.as_asgi()),
-                ])
-            ),
-        }
-)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blogd.settings")
+django.setup()
+application = get_default_application()

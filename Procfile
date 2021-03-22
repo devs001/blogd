ae449894be6a92e209e31eca38c2a55a3112f809
devs001 <devendersandhu001@gmail.com>
2021-03-22 22:10:22 +0530

    try3

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: daphne blogd.asgi:application --post $PORT --bind 0.0.0.0 -v2
+web: daphne blogd.asgi:application --post 11156 --bind 0.0.0.0 -v2
 chatworker: python manage.py runworker --settings-blogd.settings -v2
--- blogd/asgi.py
+++ blogd/asgi.py
@@ -1,8 +1,26 @@
 
+from django.conf.urls import url,re_path
+from django.core.asgi import get_asgi_application
 import os
-import django
-from channels.routing import get_default_application
+# Fetch Django ASGI application early to ensure AppRegistry is populated
+# before importing consumers and AuthMiddlewareStack that may import ORM
+# models.
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+django_asgi_app = get_asgi_application()
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blogd.settings")
-django.setup()
-application = get_default_application()
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+from .users import consumers
+application = ProtocolTypeRouter({
+    # Django's ASGI application to handle traditional HTTP requests
+    "http": django_asgi_app,
+
+    # WebSocket chat handler
+    "websocket": AuthMiddlewareStack(
+        URLRouter([
+            re_path(r'ws/userschatroom/(?P<Susername>[\w@.]+)/(?P<Rusername>[\w@.]+)/$', consumers.ChatConsumer.as_asgi()),
+            re_path(r'ws/notifications/$', consumers.ChatConsumer.as_asgi()),
+        ])
+    ),
+})
